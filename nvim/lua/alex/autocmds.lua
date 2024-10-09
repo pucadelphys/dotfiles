b171ee92deaf7a911cc9ff9bdcfa040d78db6e9a
Alejandro Barcenas <abfemat01@gmail.com>
2024-10-09 13:20:07 -0600

    new config name neovim

---
+++ nvim/lua/alex/autocmds.lua
@@ -0,0 +1,71 @@
+local create = vim.api.nvim_create_autocmd
+
+create({"FileChangedRO"}, {
+    pattern = {"*"},
+    callback = function() vim.g.autosave = false end
+})
+
+create({"BufEnter", "BufRead",}, {
+    pattern ={ "vimwiki", "*.wiki" },
+    callback = function ()
+        vim.opt.colorcolumn='0'
+        vim.opt.spelllang = 'en_us'
+        vim.opt.spell = true
+    end
+})
+
+create({
+    "InsertLeave",
+    -- "TextChanged"
+}, {
+    pattern = {"*"},
+    callback = function() AutoSave() end
+})
+
+create({"BufEnter", "BufRead"}, {
+        pattern = {"*"},
+    callback = function()
+       if vim.bo.filetype == "" then
+            vim.o.showbreak = ''
+        end
+    end
+})
+
+create("FileType", {
+    pattern = { "help", "text", "markdown" },
+    callback = function ()
+        vim.opt.colorcolumn='0'
+        vim.opt.showbreak = ''
+    end
+})
+
+create("FileType", {
+    pattern = {"tex"},
+    callback = function()
+        vim.opt.showbreak = ''
+        vim.opt.spell = true
+    end
+})
+
+create("FileType", {
+    pattern = {"r"},
+    callback = function()
+        vim.keymap.set("n", "<S-Right>", "<cmd>vertical resize 83<cr>")
+        vim.keymap.set("i", "<<", function ()
+            local pos = vim.api.nvim_win_get_cursor(0)
+            local curline = vim.api.nvim_buf_get_lines(0, pos[1] - 1, pos[1], false)[1]
+            return curline:sub(pos[2], pos[2]) == ' ' and '<- ' or ' <- '
+        end, {expr = true})
+        vim.keymap.set("i", "||", function ()
+            local pos = vim.api.nvim_win_get_cursor(0)
+            local curline = vim.api.nvim_buf_get_lines(0, pos[1] - 1, pos[1], false)[1]
+            return curline:sub(pos[2], pos[2]) == ' ' and '|> ' or ' |> '
+        end, {expr = true})
+        vim.keymap.set("i", ">>", function ()
+            local pos = vim.api.nvim_win_get_cursor(0)
+            local curline = vim.api.nvim_buf_get_lines(0, pos[1] - 1, pos[1], false)[1]
+            return curline:sub(pos[2], pos[2]) == ' ' and '%>% ' or ' %>% '
+        end, {expr = true})
+            vim.g.r_indent_align_args = 0
+    end
+})
---
+++ nvim/lua/alex/plugins/treesitter.lua
@@ -0,0 +1,52 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                ensure_installed = {
+                    "c", "lua", "vim", "vimdoc", "query", "html",
+                    "python", "r", "markdown", "markdown_inline",
+                    "rnoweb", "yaml",
+                },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = false },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        -- init_selection = "gnn",
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm,",
+                    },
+                },
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true, -- jump forward
+                        keymaps = {
+                            ["af"] = "@function.outer", -- defined in textobjects.scm
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        selection_modes = {
+                            ["@parameter.outer"] = "v", -- charwise
+                            ["@function.outer"] = "V", -- linewise
+                            ["@class.outer"] = "<c-v>", -- blockwise
+                        }, -- Can also be a function which gets passed a table with the keys
+                        -- * query_string: eg '@function.inner'
+                        -- * method: eg 'v' or 'o'
+                        -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                        include_surrounding_whitespace = true, --default false canalso be a function thatreturns true or false
+                    },
+                },
+            })
+        end,
+    }, -- Use the tree-sitter interface
+    "nvim-treesitter/nvim-treesitter-textobjects",
+}
